c3200cb0f11e8313d4444ea8e53c4e1bebedac56
sumitabha4 <sumitabha4@gmail.com>
2025-03-09 06:28:30 +0000

    basic directory stucture and github base route created

---
+++ src/app/api/github/route.js
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server';
+
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+const GITHUB_USERNAME = process.env.GITHUB_USERNAME;
+
+export async function GET() {
+  try {
+    
+    const userResponse = await fetch(`https://api.github.com/users/${GITHUB_USERNAME}`, {
+      headers: { Authorization: `token ${GITHUB_TOKEN}` }
+    });
+    const userData = await userResponse.json();
+
+    
+    const reposResponse = await fetch(`https://api.github.com/users/${GITHUB_USERNAME}/repos`, {
+      headers: { Authorization: `token ${GITHUB_TOKEN}` }
+    });
+    const reposData = await reposResponse.json();
+
+    return NextResponse.json({
+      avatar: userData.avatar_url,
+      followers: userData.followers,
+      following: userData.following,
+      repositories: reposData.map(repo => ({ name: repo.name, description: repo.description, language: repo.language, url: repo.html_url, api_url: repo.url }))
+    });
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
